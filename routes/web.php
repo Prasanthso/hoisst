3d7cd225124c68f40a961c931bdd2ff8023f3c14
kirandeep <kiran60704@gmail.com>
2024-12-10 11:37:43 +0530

    login

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,31 @@
 <?php
 
+// use Illuminate\Support\Facades\Route;
+
+
+
+// Route::get('/', function () {
+//     return view('landingPage');
+// });
+
+// Route::get('/landing',[LoginController::class])->name('landing');
+
+// Route::view('/login', 'LoginController.login')->name('login');
+
+
+// use App\Http\Controllers\LoginController;
+
+// Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.form');
+// Route::post('/login', [LoginController::class, 'verifyLogin'])->name('login.verify');
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Landing Page
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('landingPage');
+})->name('landing');
+
+// Login Routes
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login'); // Serves the login form
+Route::post('/login', [LoginController::class, 'verifyLogin'])->name('login.verify'); // Verifies credentials
