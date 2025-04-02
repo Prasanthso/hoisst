4ed80fc5f56aebae490a41d67a51d8d57fca2dec
Prasanthso <prasanthsubramaniyan945@gmail.com>
2025-04-02 18:06:13 +0530

    header

--- resources/views/layouts/headerNav.blade.php
+++ resources/views/layouts/headerNav.blade.php
@@ -288,7 +288,7 @@
                 </a>
             </li>
 
-            <li class="nav-item">
+            <!-- <li class="nav-item">
                 <a class="nav-link" href="/permission" style="background-color: rgb(186, 234, 249); font-size:17px;">
                     {{-- <i class="bi bi-grid"></i> --}}
                     <img src="/assets/img/dashboard_pic.svg" alt="dashboard Icon" style="width: 1.2em; height: auto; margin-right: 4px;">
@@ -302,7 +302,7 @@
                     <img src="/assets/img/dashboard_pic.svg" alt="dashboard Icon" style="width: 1.2em; height: auto; margin-right: 4px;">
                     <span>Role</span>
                 </a>
-            </li>
+            </li> -->
 
         </ul>
     </aside>
