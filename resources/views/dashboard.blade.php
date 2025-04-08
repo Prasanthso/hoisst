162d8abafac14a0e5228ad078acdce20c3e2906f
Nathiya <nathiyaes20@gmail.com>
2025-04-08 13:16:25 +0530

    ch

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -254,12 +254,12 @@
                 <!-- End Right side columns -->
     </section>
     <section class="section dashboard">
-        <div class="container mt-4">
+        {{-- <div class="container mt-4">
             <h5>Do you want to integrate WhatsApp API?</h5>
-            <button class="btn btn-success me-2">Yes</button>
-            {{-- <button class="btn btn-success me-2" onclick="window.location.href='{{ route('twilio.keys') }}'">Yes</button> --}}
+
+            <button class="btn btn-success me-2" onclick="window.location.href='{{ route('twilio.keys') }}'">Yes</button>
             <button class="btn btn-danger" onclick="alert('Thanks for information.')">No</button>
-        </div>
+        </div> --}}
         {{-- <div class="container mt-4">
             <h5>Do you want to activate WhatsApp messages?</h5>
             <button class="btn btn-success me-2" onclick="window.location.href='{{ route('whatsapp') }}'">Yes</button>
