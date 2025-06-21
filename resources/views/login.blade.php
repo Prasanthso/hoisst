86df86e4da343d8872c21e6ece1419647407b5a2
Nathiya <nathiyaes20@gmail.com>
2025-06-21 12:19:39 +0530

    check

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -167,7 +167,7 @@
                         required>
                     <div class="form-check mt-2">
                         <input type="checkbox" class="form-check-input" id="showPassword" onclick="togglePassword()">
-                        <label class="form-check-label" for="showPassword">Show Password</label>
+                        <label class="form-check-label" for="showPassword">Show Passwords</label>
                     </div>
                 </div>
 
